987ee232733cd0c0bf96412664210892ca4f6efc
Papa Ray <disciple.ray@icloud.com>
2018-01-10 16:14:44 +0800

    add WeatherData.java

---
+++ src/headfirst/patterns/observer/WeatherData.java
@@ -0,0 +1,13 @@
+package headfirst.patterns.observer;
+
+/**
+ * Desc
+ *
+ * @author Papa Ray
+ * Created on 2018-01-10
+ */
+public class WeatherData {
+    private float temperature;
+    private float humidity;
+    private float pressure;
+}
